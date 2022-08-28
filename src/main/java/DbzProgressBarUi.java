73d0538bc25cfb018721d97fae77f7a1d94d7958
Ege Inanc <ege.inanc1991@gmail.com>
2022-08-28 15:32:55 +0200

    include jindujun

--- src/main/java/DbzProgressBarUi.java
+++ src/main/java/DbzProgressBarUi.java
@@ -13,27 +13,6 @@ import java.awt.geom.RoundRectangle2D;
 
 public class DbzProgressBarUi extends BasicProgressBarUI {
 
-    private void paint(Graphics g, JComponent c, boolean determinate) {
-        if (determinate) {
-            super.paintDeterminate(g, c);
-            Icon icon = new ImageIcon(getClass().getResource("goku_kamehameha.gif"));
-            icon.paintIcon(progressBar, g, 0, 0);
-        } else {
-            super.paintIndeterminate(g, c);
-            Icon icon = new ImageIcon(getClass().getResource("goku_ki.gif"));
-            icon.paintIcon(progressBar, g, 0, 0);
-        }
-
-
-        progressBar.setForeground(UIUtil.getBoundsColor());
-
-        // border
-        final int width = progressBar.getWidth();
-        int height = progressBar.getPreferredSize().height;
-
-        drawBorder((Graphics2D) g, width, height);
-    }
-
     private void drawBorder(Graphics2D g, int width, int height) {
         RoundRectangle2D rectangle = getRoundRectangle(width, height);
 
@@ -59,12 +38,30 @@ public class DbzProgressBarUi extends BasicProgressBarUI {
 
     @Override
     protected void paintIndeterminate(Graphics g, JComponent c) {
-        paint(g, c, false);
-    }
+        super.paintIndeterminate(g, c);
+        Icon icon = new ImageIcon(getClass().getResource("goku_ki.gif"));
+        icon.paintIcon(progressBar, g, 0, 0);
+
+
+        progressBar.setForeground(UIUtil.getBoundsColor());
+
+        // border
+        final int width = progressBar.getWidth();
+        int height = progressBar.getPreferredSize().height;
+
+        drawBorder((Graphics2D) g, width, height);    }
 
     @Override
     protected void paintDeterminate(Graphics g, JComponent c) {
-        paint(g, c, true);
+        super.paintDeterminate(g, c);
+
+        Icon clouds = new ImageIcon(getClass().getResource("clouds.png"));
+        clouds.paintIcon(progressBar, g, 0, 0);
+
+        Icon icon = new ImageIcon(getClass().getResource("jindujun.png"));
+        int amountFull = getAmountFull(progressBar.getInsets(), progressBar.getWidth(), progressBar.getHeight());
+        icon.paintIcon(progressBar, g, amountFull <= 10 ? 20 - icon.getIconWidth(): amountFull - icon.getIconWidth(), 0);
+
     }
 
     @Override
