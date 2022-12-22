7a6d62c27d1c649fb6a0f16f8dfe28ed8dbfadcb
egeinanc <ege.inanc1991@gmail.com>
2022-12-22 08:41:14 +0100

    #15 full width on load start (#19)

--- src/main/java/DbzProgressBarUi.java
+++ src/main/java/DbzProgressBarUi.java
@@ -83,8 +83,12 @@ public class DbzProgressBarUi extends BasicProgressBarUI {
 
         int amountFull = getAmountFull(progressBar.getInsets(), progressBar.getWidth(), progressBar.getHeight());
 
+        int rightCornerPosition = amountFull - imageIcon.getIconWidth();
+
+        int iconPosition = rightCornerPosition + determinateSprite.getOffset();
+
         int yAxis = height > imageIcon.getIconHeight() ? (height - imageIcon.getIconHeight()) / 2 : 0;
-        imageIcon.paintIcon(progressBar, g, amountFull - imageIcon.getIconWidth(), Math.round(yAxis));
+        imageIcon.paintIcon(progressBar, g, iconPosition, Math.round(yAxis));
 
     }
 
