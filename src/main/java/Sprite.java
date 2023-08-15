9a18dcd36ed4211455995117c15886fae3ff1d9f
Ege Inanc <ege.inanc1991@gmail.com>
2023-08-15 09:37:18 +0200

    new sprite

--- src/main/java/Sprite.java
+++ src/main/java/Sprite.java
@@ -7,7 +7,8 @@ import javax.swing.*;
 @AllArgsConstructor
 public enum Sprite {
     GOKU_JINDUJUN(2, new ImageIcon(DbzProgressBarUi.getResource("jindujun.png")), true, true, 30),
-    GOKU_FLYING(3, new ImageIcon(DbzProgressBarUi.getResource("flying_goku.png")), true, false, null),
+    GOKU_FLYING(3, new ImageIcon(DbzProgressBarUi.getResource("flying_goku.png")), true, false, 0),
+    GOKU_KI(3, new ImageIcon(DbzProgressBarUi.getResource("goku_ki.gif")), false, true, 0),
 ;
     private final int id;
     private final ImageIcon icon;
