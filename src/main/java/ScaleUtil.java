4bca75b3634ea49a68d67312688be1eb4afadf18
Ege Inanc <ege.inanc1991@gmail.com>
2023-09-02 12:21:16 +0200

    fix: height issue

--- src/main/java/ScaleUtil.java
+++ src/main/java/ScaleUtil.java
@@ -1,8 +1,10 @@
+import com.intellij.ui.paint.PaintUtil;
+import com.intellij.ui.scale.ScaleContext;
 import com.intellij.util.ui.ImageUtil;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
 
 public class ScaleUtil {
 
@@ -15,7 +17,8 @@ public class ScaleUtil {
         int newWidth = (int) (heightToUse / ratio);
 
         Image originalimage = originalicon.getImage();
-        BufferedImage scaledImage = ImageUtil.createImage(newWidth, heightToUse, BufferedImage.TYPE_INT_ARGB);
+        ScaleContext sc = ScaleContext.createIdentity();
+        BufferedImage scaledImage = ImageUtil.createImage(sc, newWidth, heightToUse, BufferedImage.TYPE_INT_ARGB, PaintUtil.RoundingMode.CEIL);
 
         Graphics2D g = scaledImage.createGraphics();
         g.drawImage(originalimage, 0, 0, newWidth, heightToUse, null);
